58623a95fc8299e86f24329f1d9c4de4e9890037
Edenilson <edenilsonju10@gmail.com>
2024-05-03 18:54:36 -0300

    Add project files.

---
+++ FibonacciPilha/PilhaNumero.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciPilha
+{
+    internal class PilhaNumero
+    {
+        Numero? topo;
+        int tamanho;
+
+        public PilhaNumero()
+        {
+            topo = null;
+            tamanho = 0;
+        }
+
+        public void Push(Numero aux)
+        {
+            if (IsEmpty())
+            {
+                topo = aux;
+            }
+            else
+            {
+                aux.SetAnterior(topo);
+                topo = aux;
+            }
+
+            tamanho++;
+        }
+
+        public Numero? Pop()
+        {
+            Numero? removido = null;
+
+            if (!IsEmpty())
+            {
+                removido = topo;
+                topo = topo.GetAnterior();
+
+                tamanho--;
+            }
+            removido.SetAnterior(null);
+            return removido;
+        }
+
+        public int GetTamanho()
+        {
+            return tamanho;
+        }
+
+        public bool IsEmpty()
+        {
+            return topo == null;
+        }
+
+        public void Print()
+        {
+
+            if (IsEmpty())
+            {
+                Console.WriteLine("-->Pilha vazia!");
+            }
+            else
+            {
+                Numero aux = topo;
+
+                while (aux != null)
+                {
+                    Console.WriteLine($"--> {aux}");
+                    aux = aux.GetAnterior();
+                }
+            }
+        }
+    }
+}
---
+++ FibonacciPilha/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace FibonacciPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("====================");
+            Console.WriteLine(">>> Fibonacci <<<");
+
+            bool resultadoTry;
+            int tamanho, temp = 0, anterior = 0, atual = 1;
+            PilhaNumero pilha = new PilhaNumero();
+
+            do
+            {
+                Console.WriteLine("Digite a quantidade de numeros da sequencia:");
+                Console.Write("R: ");
+
+                resultadoTry = int.TryParse(Console.ReadLine(), out tamanho);
+
+            } while (!resultadoTry);
+
+            if (tamanho <= 0)
+            {
+                Console.WriteLine("Numero incorreto!");
+            }
+            else
+            {
+                if (tamanho >= 1)
+                    pilha.Push(new Numero(anterior));
+                if (tamanho >= 2)
+                    pilha.Push(new Numero(atual));
+                for (int i = 2; i < tamanho; i++)
+                {
+                    temp = atual + anterior;
+                    anterior = atual;
+                    atual = temp;
+
+                    pilha.Push(new Numero(atual));
+                }
+
+                Console.WriteLine("Sequencia fibonacci: ");
+                pilha.Print();
+            }
+            Console.ReadKey();
+        }
+    }
+}
